496196aa010a9346afb8e4c1b280a3b39e28b9d4
Willian Massami Watanabe <watinha@gmail.com>
2011-05-25 23:06:18 -0300

    Change html_extractor class to use urllib2 redirect functionality and creates a new method for identifying the encoding

--- libs/html_utilities.py
+++ libs/html_utilities.py
@@ -6,45 +6,21 @@
 Function responsible for acquiring the HTML code for an specific webpage
 """
 
-import httplib
+import urllib2
 import re
 
 def html_extractor(url):
-  """ Recursion function that follows HTTP moved out wepages """  
-
-  [domain, url_add] = parse_url(url)
-  if (domain == ""):
+  """ Uses urllib2 to follow redirects """
+  if (len(url) == 0):
     return ""
 
-  conn = httplib.HTTPConnection(domain)
-  conn.request("GET", url_add)
-  response = conn.getresponse()
+  request = urllib2.Request(url)
+  opener = urllib2.build_opener()
+  response = opener.open(request)
+
   content = response.read()
 
-  headers = response.getheaders()
-  content_type = filter (lambda x: x[0] == 'content-type', headers)
-  try: 
-    re_match = re.search("charset=((.)+)", content_type[0][1])
-    encoding = re_match.group(1)
-  except AttributeError, IndexError:
-    try:
-      re_match = re.search("charset=((.)+)", content)
-      encoding = re_match.group(1)
-    except AttributeError:
-      # tries defalt encoding
-      encoding = "ISO-8859-1"
-
-  if (response.status == 302 or response.status == 303 or response.status == 301):
-    location = filter (lambda x: x[0] == 'location', headers)
-    try:
-      return html_extractor(location[0][1])
-    except:
-      try:
-        re_match = re.search("<A HREF=(\"|')((.)+)('|\")>", content)
-        new_url = re_match.group(2)
-        return html_extractor(new_url)
-      except AttributeError:
-        return content
+  encoding = get_encoding(response.headers["content-type"], content)
 
   return content.decode(encoding)
 
@@ -53,9 +29,24 @@ def parse_url(url):
     return ["", ""]
   url_array = filter(lambda x: x[0:4] != "http" and x != "", url.split("/"))
   domain = url_array[0]
-  
+
   if(len(url_array) == 1):
     url_add = ""
   else:
     url_add = "/" + reduce(lambda x, y: x + "/" + y, url_array[1:])
   return [domain, url_add]
+
+def get_encoding(content_type, content):
+  """ Accepts only UTF-8 and ISO-8859-1 """
+  try:
+    re_match = re.search("charset=((.)+)", content_type)
+    encoding = re_match.group(1)
+  except AttributeError, IndexError:
+    try:
+      re_match = re.search("charset=((.)+)(\'|\")", content)
+      encoding = re_match.group(1)
+    except AttributeError, IndexError:
+      # tries defalt encoding
+      encoding = "ISO-8859-1"
+
+  return encoding
--- tests/test_html_utilities.py
+++ tests/test_html_utilities.py
@@ -26,6 +26,25 @@ class HtmlUtilitiesTest(unittest.TestCase):
 
   def test_encoding_issues(self):
     test_url = "http://www.uol.com"
+    response = html_utilities.html_extractor(test_url)
+    self.assertTrue(len(response) > 15)
 
-    self.assertTrue(len(html_utilities.html_extractor(test_url)) > 15)
+  def test_get_encoding_utf8(self):
+    test_content_type_1 = "text/html; charset=ISO-8859-1"
+    test_content_1 = "<html><body>Text in html content</body></html>"
+    test_content_type_2 = "text/html"
+    test_content_2 = "<html><meta content='text/html; charset=UTF-8'></html>"
+    test_content_type_3 = "text/html; charset=UTF-8"
+    test_content_3 = "<html><meta content='text/html; charset=UTF-8'></html>"
 
+    test_content_type_4 = "text/html;"
+    test_content_4 = "<html><meta content='text/html; charset=ISO-8859-1'></html>"
+
+    test_content_type_5 = "text/html;"
+    test_content_5 = "<html><meta content='text/html; charset=ISO-8859-1'></html>"
+
+    self.assertEquals("ISO-8859-1", html_utilities.get_encoding(test_content_type_1, test_content_1))
+    self.assertEquals("UTF-8", html_utilities.get_encoding(test_content_type_2, test_content_2))
+    self.assertEquals("UTF-8", html_utilities.get_encoding(test_content_type_3, test_content_3))
+    self.assertEquals("ISO-8859-1", html_utilities.get_encoding(test_content_type_4, test_content_4))
+    self.assertEquals("ISO-8859-1", html_utilities.get_encoding(test_content_type_5, test_content_5))
